d942b8652628cc1d5d958f7dd7b7cd73c9ef4146
NAMBO01 <93188330+NAMBO01@users.noreply.github.com>
2022-11-14 11:00:34 +0700

    test 3

--- th_php/view/trangchu.php
+++ th_php/view/trangchu.php
@@ -3,13 +3,13 @@
 
 <head>
     <!-- include head -->
-    <?php include_once("widgets/head.php"); ?>
+    <?php include_once("../widgets/head.php"); ?>
     <!-- END include head -->
 
 </head>
 
 <body>
-    <?php include_once("widgets/header.php"); ?>
+    <?php include_once("../widgets/header.php"); ?>
 
     <div class="container">
         <div class="shoes-grid">
@@ -20,7 +20,7 @@
                             <article style="position: absolute; width: 100%; opacity: 0;">
                                 <div class="banner-matter">
                                     <div class="col-md-5 banner-bag">
-                                        <img class="img-responsive " src="images/bag.jpg" alt=" " />
+                                        <img class="img-responsive " src="../images/bag.jpg" alt=" " />
                                     </div>
                                     <div class="col-md-7 banner-off">
                                         <h2>FLAT 50% 0FF</h2>
@@ -36,7 +36,7 @@
                             <article style="position: absolute; width: 100%; opacity: 0;">
                                 <div class="banner-matter">
                                     <div class="col-md-5 banner-bag">
-                                        <img class="img-responsive " src="images/bag1.jpg" alt=" " />
+                                        <img class="img-responsive " src="../images/bag1.jpg" alt=" " />
                                     </div>
                                     <div class="col-md-7 banner-off">
                                         <h2>FLAT 50% 0FF</h2>
@@ -52,7 +52,7 @@
                             <article style="position: absolute; width: 100%; opacity: 0;">
                                 <div class="banner-matter">
                                     <div class="col-md-5 banner-bag">
-                                        <img class="img-responsive " src="images/bag.jpg" alt=" " />
+                                        <img class="img-responsive " src="../images/bag.jpg" alt=" " />
                                     </div>
                                     <div class="col-md-7 banner-off">
                                         <h2>FLAT 50% 0FF</h2>
@@ -73,7 +73,7 @@
                 <li><a href="#" class="">1</a></li>
                 <li><a href="#" class="">2</a></li>
             </ul>
-            <script src="js/jquery.wmuSlider.js"></script>
+            <script src="../js/jquery.wmuSlider.js"></script>
             <script>
                 $('.example1').wmuSlider();
             </script>
@@ -92,7 +92,7 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, </p>
                     <span class="on-get">GET NOW</span>
                 </div>
-                <img class="img-responsive shoe-left" src="images/sh.jpg" alt=" " />
+                <img class="img-responsive shoe-left" src="../images/sh.jpg" alt=" " />
 
                 <div class="clearfix"> </div>
 
@@ -106,7 +106,7 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, </p>
                     <span class="on-get">GET NOW</span>
                 </div>
-                <img class="img-responsive shoe-left" src="images/wa.jpg" alt=" " />
+                <img class="img-responsive shoe-left" src="../images/wa.jpg" alt=" " />
 
                 <div class="clearfix"> </div>
             </div>
@@ -118,7 +118,7 @@
     </div>
     <div class="product-left">
         <div class="col-md-4 chain-grid">
-            <a href="single.html"><img class="img-responsive chain" src="images/ch.jpg" alt=" " /></a>
+            <a href="single.html"><img class="img-responsive chain" src="../images/ch.jpg" alt=" " /></a>
             <span class="star"> </span>
             <div class="grid-chain-bottom">
                 <h6><a href="single.html">Lorem ipsum dolor</a></h6>
@@ -145,7 +145,7 @@
             </div>
         </div>
         <div class="col-md-4 chain-grid">
-            <a href="single.html"><img class="img-responsive chain" src="images/ba.jpg" alt=" " /></a>
+            <a href="single.html"><img class="img-responsive chain" src="../images/ba.jpg" alt=" " /></a>
             <span class="star"> </span>
             <div class="grid-chain-bottom">
                 <h6><a href="single.html">Lorem ipsum dolor</a></h6>
@@ -172,7 +172,7 @@
             </div>
         </div>
         <div class="col-md-4 chain-grid grid-top-chain">
-            <a href="single.html"><img class="img-responsive chain" src="images/bo.jpg" alt=" " /></a>
+            <a href="single.html"><img class="img-responsive chain" src="../images/bo.jpg" alt=" " /></a>
             <span class="star"> </span>
             <div class="grid-chain-bottom">
                 <h6><a href="single.html">Lorem ipsum dolor</a></h6>
@@ -206,7 +206,7 @@
     </div>
     <div class="product-left">
         <div class="col-md-4 chain-grid">
-            <a href="single.html"><img class="img-responsive chain" src="images/bott.jpg" alt=" " /></a>
+            <a href="single.html"><img class="img-responsive chain" src="../images/bott.jpg" alt=" " /></a>
             <span class="star"> </span>
             <div class="grid-chain-bottom">
                 <h6><a href="single.html">Lorem ipsum dolor</a></h6>
@@ -233,7 +233,7 @@
             </div>
         </div>
         <div class="col-md-4 chain-grid">
-            <a href="single.html"><img class="img-responsive chain" src="images/bottle.jpg" alt=" " /></a>
+            <a href="single.html"><img class="img-responsive chain" src="../images/bottle.jpg" alt=" " /></a>
             <span class="star"> </span>
             <div class="grid-chain-bottom">
                 <h6><a href="single.html">Lorem ipsum dolor</a></h6>
@@ -260,7 +260,7 @@
             </div>
         </div>
         <div class="col-md-4 chain-grid grid-top-chain">
-            <a href="single.html"><img class="img-responsive chain" src="images/baa.jpg" alt=" " /></a>
+            <a href="single.html"><img class="img-responsive chain" src="../images/baa.jpg" alt=" " /></a>
             <span class="star"> </span>
             <div class="grid-chain-bottom">
                 <h6><a href="single.html">Lorem ipsum dolor</a></h6>
@@ -290,89 +290,11 @@
     </div>
     <div class="clearfix"> </div>
     </div>
-    <div class="sub-cate">
-        <div class=" top-nav rsidebar span_1_of_left">
-            <h3 class="cate">CATEGORIES</h3>
-            <ul class="menu">
-                <li class="item1"><a href="#">Curabitur sapien<img class="arrow-img" src="images/arrow1.png" alt="" /> </a>
-                    <ul class="cute">
-                        <li class="subitem1"><a href="product.html">Cute Kittens </a></li>
-                        <li class="subitem2"><a href="product.html">Strange Stuff </a></li>
-                        <li class="subitem3"><a href="product.html">Automatic Fails </a></li>
-                    </ul>
-                </li>
-                <li class="item2"><a href="#">Dignissim purus <img class="arrow-img " src="images/arrow1.png" alt="" /></a>
-                    <ul class="cute">
-                        <li class="subitem1"><a href="product.html">Cute Kittens </a></li>
-                        <li class="subitem2"><a href="product.html">Strange Stuff </a></li>
-                        <li class="subitem3"><a href="product.html">Automatic Fails </a></li>
-                    </ul>
-                </li>
-                <li class="item3"><a href="#">Ultrices id du<img class="arrow-img img-arrow" src="images/arrow1.png" alt="" /> </a>
-                    <ul class="cute">
-                        <li class="subitem1"><a href="product.html">Cute Kittens </a></li>
-                        <li class="subitem2"><a href="product.html">Strange Stuff </a></li>
-                        <li class="subitem3"><a href="product.html">Automatic Fails</a></li>
-                    </ul>
-                </li>
-                <li class="item4"><a href="#">Cras iacaus rhone <img class="arrow-img img-left-arrow" src="images/arrow1.png" alt="" /></a>
-                    <ul class="cute">
-                        <li class="subitem1"><a href="product.html">Cute Kittens </a></li>
-                        <li class="subitem2"><a href="product.html">Strange Stuff </a></li>
-                        <li class="subitem3"><a href="product.html">Automatic Fails </a></li>
-                    </ul>
-                </li>
-                <li>
-                    <ul class="kid-menu">
-                        <li><a href="product.html">Tempus pretium</a></li>
-                        <li><a href="product.html">Dignissim neque</a></li>
-                        <li><a href="product.html">Ornared id aliquet</a></li>
-                    </ul>
-                </li>
-                <ul class="kid-menu ">
-                    <li><a href="product.html">Commodo sit</a></li>
-                    <li><a href="product.html">Urna ac tortor sc</a></li>
-                    <li><a href="product.html">Ornared id aliquet</a></li>
-                    <li><a href="product.html">Urna ac tortor sc</a></li>
-                    <li><a href="product.html">Eget nisi laoreet</a></li>
-                    <li><a href="product.html">Faciisis ornare</a></li>
-                    <li class="menu-kid-left"><a href="contact.html">Contact us</a></li>
-                </ul>
-            </ul>
-        </div>
-        <!--initiate accordion-->
-        <script type="text/javascript">
-            $(function() {
-                var menu_ul = $('.menu > li > ul'),
-                    menu_a = $('.menu > li > a');
-                menu_ul.hide();
-                menu_a.click(function(e) {
-                    e.preventDefault();
-                    if (!$(this).hasClass('active')) {
-                        menu_a.removeClass('active');
-                        menu_ul.filter(':visible').slideUp('normal');
-                        $(this).addClass('active').next().stop(true, true).slideDown('normal');
-                    } else {
-                        $(this).removeClass('active');
-                        $(this).next().stop(true, true).slideUp('normal');
-                    }
-                });
+    <?php include_once("../widgets/categories.php"); ?>
 
-            });
-        </script>
-        <div class=" chain-grid menu-chain">
-            <a href="single.html"><img class="img-responsive chain" src="images/wat.jpg" alt=" " /></a>
-            <div class="grid-chain-bottom chain-watch">
-                <span class="actual dolor-left-grid">300$</span>
-                <span class="reducedfrom">500$</span>
-                <h6><a href="single.html">Lorem ipsum dolor</a></h6>
-            </div>
-        </div>
-        <a class="view-all all-product" href="product.html">VIEW ALL PRODUCTS<span> </span></a>
-    </div>
     <div class="clearfix"> </div>
     </div>
-    <?php include_once("widgets/head.php"); ?>
+    <?php include_once("../widgets/footer.php"); ?>
 
 </body>
 
